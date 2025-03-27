40adaa094c6b433de8f234ada7530845365f6411
Gabriel Sá <gabriel10043@live.com>
2025-03-27 11:32:13 -0300

    add debug macro

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 chip: chip8.o main.c
 	gcc -o chip chip8.o main.c
 
+debug: chip8.o main.c
+	gcc -D DEBUG -o chip chip8.o main.c
+
 chip8.o: chip8.c chip8.h
 	gcc -c chip8.c
 
--- main.c
+++ main.c
@@ -5,6 +5,40 @@
 #include <time.h>
 
 
+#ifdef DEBUG
+	#define debug_fn(c)\
+		for(int v = 0; v < 16; v+=4){\
+			printf("V%d: %x ", v, c->V[v]);\
+			printf("V%d: %x ", v+1, c->V[v+1]);\
+			printf("V%d: %x ", v+2, c->V[v+2]);\
+			printf("V%d: %x\n", v+3, c->V[v+3]);\
+		}\
+		for(int mem = 0; mem < 4096; mem+=256){\
+			printf("%x ", c->memory[mem]);	\
+			printf("%x ", c->memory[mem+1]);	\
+			printf("%x ", c->memory[mem+2]);	\
+			printf("%x ", c->memory[mem+3]);	\
+			printf("%x ", c->memory[mem+4]);	\
+			printf("%x ", c->memory[mem+5]);	\
+			printf("%x ", c->memory[mem+6]);	\
+			printf("%x ", c->memory[mem+7]);	\
+			printf("%x ", c->memory[mem+8]);	\
+			printf("%x ", c->memory[mem+9]);	\
+			printf("%x ", c->memory[mem+10]);	\
+			printf("%x ", c->memory[mem+11]);	\
+			printf("%x ", c->memory[mem+12]);	\
+			printf("%x ", c->memory[mem+13]);	\
+			printf("%x ", c->memory[mem+14]);	\
+			printf("%x\n", c->memory[mem+15]);	\
+		}\
+		printf("pc: %x sp: %x, opcode: %x\n", c->pc, c->sp, c->opcode)
+#else
+	#define debug_fn(c)\
+		do{\
+			}while(0)
+#endif
+
+
 int main(int argc, char** argv){
 	if(argc < 2){
 		printf("Usage: %s <rom-path>\n", argv[0]);
@@ -26,6 +60,7 @@ int main(int argc, char** argv){
 	for(int i = 0; i < 3; i++){
 		run_cycle(chip);
 	}
+	debug_fn(chip);
 	fclose(f);
 	cleanup(chip);
 	return 0;
