8670566c3e78a8b3aefedc063f63079292620f45
Gabriel Sá <gabriel10043@live.com>
2025-03-26 23:51:16 -0300

    add main file

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "chip8.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+int main(int argc, char** argv){
+	if(argc < 2){
+		printf("Usage: %s <rom-path>\n", argv[0]);
+		return 1;
+	}
+	FILE* f = fopen(argv[1], "rb");
+	if(!f){
+		printf("%s: Unable to open %s\n", argv[0], argv[1]);
+		return 1;
+	}
+	Chip* chip = initialize();
+	if(!chip){
+		printf("%s: Unexpected error\n", argv[0]);
+		fclose(f);
+		return 1;
+	}
+		
+	load_game(chip, f);
+	fclose(f);
+	cleanup(chip);
+	return 0;
+}
