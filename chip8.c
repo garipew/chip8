aeaad2a0df1d787931b413394fe7d1f13c4ecf8e
Gabriel Sá <gabriel10043@live.com>
2025-03-26 23:27:16 -0300

    add load_game()

--- chip8.c
+++ chip8.c
@@ -5,6 +5,14 @@
 #include <time.h>
 
 
+#define push(c, data) \
+	c->memory[STACK_ADDR+c->sp++] = data;
+
+
+#define pop(c) \
+	c->memory[STACK_ADDR+(--c->sp)];
+
+
 Chip* initialize(){
 	Chip* c = malloc(sizeof(*c));
 	if(!c){
@@ -19,3 +27,20 @@ Chip* initialize(){
 	srand(time(NULL));
 	return c;
 }
+
+
+void load_game(Chip* chip, FILE* stream){
+	if(c->sp+1 < STACK_SIZE){
+		push(c, (chip->pc >> 8) & 0xFF);
+		push(c, chip->pc & 0xFF);
+	} else{
+		printf("Stack Overflow.\n");
+		return;
+	}
+	for(chip->pc = 0x200; chip->pc < STACK_ADDR; chip->pc+=2){
+		fgets(chip->memory + chip->pc, 2, stream);
+	}
+	chip->pc = pop(c) | pop(c)<<8;
+}
+
+
